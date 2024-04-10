55d84cebf4ea7ce35105b3693043d58334591001
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-04-10 09:02:30 -0600

    JOptionPane test

--- Java/clasePractica/CP4/src/poliza/PolizaAuto.java
+++ Java/clasePractica/CP4/src/poliza/PolizaAuto.java
@@ -51,4 +51,5 @@ public class PolizaAuto {
         }
         return municipioSinMulta;
     }
+
 }
--- Java/clasePractica/CP4/src/poliza/PruebaPolizaAuto.java
+++ Java/clasePractica/CP4/src/poliza/PruebaPolizaAuto.java
@@ -1,5 +1,14 @@
 package poliza;
 
+import javax.swing.JOptionPane;
+
 public class PruebaPolizaAuto {
-    
+    public static void main(String[] args) {
+        // JOptionPane.showMessageDialog(null, "waos");
+        PolizaAuto instacia1 = new PolizaAuto(1234, "Toyota corolla", "MY");
+        
+        JOptionPane.showMessageDialog(null, instacia1.municipioSinMulta(), "Tus municipio", 1);
+        
+        
+    }
 }
