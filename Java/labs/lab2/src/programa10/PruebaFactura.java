105510451cd4218c2bd746ff8bff2e1e659f4e37
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-04-14 23:11:17 -0600

    all programs finished

--- Java/labs/lab2/src/programa10/PruebaFactura.java
+++ Java/labs/lab2/src/programa10/PruebaFactura.java
@@ -9,6 +9,14 @@ public class PruebaFactura {
 		
 		JOptionPane.showMessageDialog(null, String.format("Numero de pieza: %s%nDescripcion de pieza: %s%nCantidad: %d%nPrecio por articulo: %.2f%nMonto de la factura: %.2f%n",
 		factura1.getNumDePieza(), factura1.getDescripcionDePieza(), factura1.getCantidad(), factura1.getPrecioArticulo(), factura1.obtenerMontoFactura()), "Factura", 1);
+
+		factura1.setCantidad(2);
+		factura1.setDescripcionDePieza("cuchillo");
+		factura1.setNumDePieza("B5678");
+		factura1.setPrecioArticulo(128.52);
+
+		JOptionPane.showMessageDialog(null, String.format("Numero de pieza: %s%nDescripcion de pieza: %s%nCantidad: %d%nPrecio por articulo: %.2f%nMonto de la factura: %.2f%n",
+		factura1.getNumDePieza(), factura1.getDescripcionDePieza(), factura1.getCantidad(), factura1.getPrecioArticulo(), factura1.obtenerMontoFactura()), "Factura modificada", 1);
 		
 	}
 
