7ab7412070e6c1aa32bfd4b1d0d7db91e90d0d12
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-05-06 10:22:18 -0600

    advancements

---
+++ Java/labs/lab4/programa2/Conversion.java
@@ -0,0 +1,34 @@
+package programa2;
+
+import javax.swing.JOptionPane;
+
+public class Conversion {
+    double grados;
+
+    public Conversion(double grados) {
+        this.grados = grados;
+    }
+
+    public double getGrados() {
+        return grados;
+    }
+
+    public void setGrados(double grados) {
+        this.grados = grados;
+    }
+
+    public void convertirFC(){
+        this.grados = (grados * 9/5) + 32;
+    }
+
+    public void convertirCF(){
+        this.grados = (grados - 32) * 5/9;
+    }
+    public static void main(String[] args) {
+        Conversion conversion1 = new Conversion(Double.parseDouble(JOptionPane.showInputDialog("Ingrese los grados en Celsius")));
+        conversion1.convertirCF();
+        JOptionPane.showMessageDialog(null, String.format("Los grados en Fahrenheit son: %.2f", conversion1.getGrados()), "Grados en Fahrenheit", 1);
+        conversion1.convertirFC();
+        JOptionPane.showMessageDialog(null, String.format("Los grados en Celsius son: %.2f", conversion1.getGrados()), "Grados en Celsius", 1);
+    }
+}
