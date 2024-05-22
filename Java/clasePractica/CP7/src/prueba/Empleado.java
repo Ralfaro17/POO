97e07a420fe9a273382d94984544c0bf01a9c07e
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-05-21 22:53:39 -0600

    CP7 finished

---
+++ Java/clasePractica/CP7/src/prueba/Empleado.java
@@ -0,0 +1,30 @@
+package prueba;
+
+public class Empleado {
+    private final String primerNombre;
+    private final String apellidoPaterno;
+    private final String numeroSeguroSocial;
+
+    public Empleado(String primerNombre, String apellidoPaterno, String numeroSeguroSocial) {
+        this.primerNombre = primerNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.numeroSeguroSocial = numeroSeguroSocial;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getNumeroSeguroSocial() {
+        return numeroSeguroSocial;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s %s\nSeguro: %s\n", getPrimerNombre(), getApellidoPaterno(), getNumeroSeguroSocial());
+    }
+}
