87de29008a5c8359f6d418064cfc287992ff5a6e
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-05-22 10:22:50 -0600

    updated CP7 and added CP8

---
+++ Java/clasePractica/CP7/src/prueba/PruebaEmpleadoPorHora.java
@@ -0,0 +1,12 @@
+package prueba;
+
+import javax.swing.JOptionPane;
+
+public class PruebaEmpleadoPorHora {
+    public static void main(String[] args) {
+        EmpleadoPorHora empleado1 = new EmpleadoPorHora("Jose", "Fonseca", "12345678", 40, 20);
+    
+        JOptionPane.showMessageDialog(null, empleado1, "Datos empleado", 1);
+        
+    }
+}
